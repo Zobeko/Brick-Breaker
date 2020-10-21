e4778103f0e04cbef5e365466e18c0b494bedeef
Andrea.P <andrea.ponzio@telecom-sudparis.eu>
2020-10-21 17:02:45 +0200

    Ajout menu pause

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    
+
+    public void ResumeButton()
+    {
+        UIManager.instance.pauseMenuON = false;
+        Time.timeScale = 1f;
+        AudioListener.volume = 1f;
+        Cursor.visible = false;
+        this.gameObject.SetActive(false);
+    }
+
+    public void RetryButton()
+    {
+        UIManager.instance.pauseMenuON = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuPanel = null;
+    public bool pauseMenuON = false;
+
+    public static UIManager instance;
+    void Start()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        pauseMenuON = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Gère affichage et fermeture du menu pause
+        if(Input.GetKeyDown(KeyCode.Escape) && !pauseMenuON)
+        {
+            pauseMenuPanel.SetActive(true);
+            pauseMenuON = true;
+            Time.timeScale = 0f;
+            AudioListener.volume = 0f;
+            Cursor.visible = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && pauseMenuON)
+        {
+            pauseMenuPanel.SetActive(false);
+            pauseMenuON = false;
+            Time.timeScale = 1f;
+            AudioListener.volume = 1f;
+            Cursor.visible = false;
+        }
+
+
+    }
+}
